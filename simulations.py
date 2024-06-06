aadc42dcc6462bbdf39a83624593934f801234e4
Juliette Luiselli <juliette.luiselli@inria.fr>
2024-06-06 15:40:49 -0400

    parallel run of multiple simulations made easy

--- simulations.py
+++ simulations.py
@@ -1,36 +1,54 @@
-from recomb_model import *
+from recomb_model_v2 import *
 import random as random
 import os as os
+import multiprocessing
+from execo_engine import sweep
 
-final_time = 50000
-population_sizes = [500, 200, 1000]
-chrsm_lengths = [10000, 5000, 15000]
-segment_lengths = [100, 20, 500]
+final_time = 800
 
-for j in range(5): #We repeat each conditions 5 times
-    for popsize in [1000]:
-        for chrsm_len in [5000,15000]:
-            if popsize==1000 and chrsm_len==15000:
-                continue
-            for seg_len in segment_lengths:
-                pop = Population(popsize,final_time, chrsm_len)
-                while pop.T < final_time:
-                    if (pop.T%500)==0:
-                        print("current forward time", pop.T)
-                    pop.forward_step()
+parameters = {
+    "popsize" : [20, 100, 1000, 4000],
+    "chrsm_len" : [10000, 100000, 1000000],
+    "recomb_rate" : [1,10,36],
+    "run_id" : [10,11,12,13,14]
+}
 
-                for indiv_id in range(10): # We take 10 random individuals to start our reconstructions
-                    filename = "data/pop_size-"+str(popsize)+"-chrsm_len-"+str(chrsm_len)+"-seg_len-"+str(seg_len)+"-rep-"+str(j*10+indiv_id)+".json"
-                    print(filename)
-                    if os.path.isfile(filename):
-                        start_seg = random.randint(0, chrsm_len - seg_len)
-                        lin = Lineage(indiv_id, random.randint(0,1), start_seg, start_seg+seg_len, pop)
-                        while (lin.back_time<final_time-1 and
-                            ((not lin.has_separated)
-                            or (lin.has_separated and len(lin.lineage[lin.back_time])>1))):
-                            lin.backward_step()
-                            lin.check_fused_segments()
-                        lin.write_data(filename)
-                    else:
-                        print("already computed")
 
+sweeps = sweep(parameters)
+print(sweeps)
+
+
+
+def simulation(d):
+    popsize = d["popsize"]
+    chrsm_len = d["chrsm_len"]
+    recomb_rate = d["recomb_rate"]
+    run_id = d["run_id"]
+
+    filename = "ghosts/pop_size-"+str(popsize)+"-chrsm_len-"+str(chrsm_len)+"-recomb-"+str(recomb_rate)+\
+    "-rep-"+str(run_id)+".json"
+    print(filename)
+    if not os.path.isfile(filename):
+        pop = Population(popsize, chrsm_len, recomb_rate/chrsm_len)
+        for i in range(pop.size):
+            pop.individuals[i] = Individual(i, None, None, chrsm_len, pop.r, None)
+
+        lin = Lineage(pop)
+        # lin  = Lineage(pop, [Segment(0, random.randint(0,1), start_seg, start_seg+seg_len-1)])
+        while (lin.back_time<final_time-1):
+            if lin.back_time%100==0:
+                print(f"T={lin.back_time}  S={len(lin.cur_segments)}   N={len(lin.pop.individuals)}  Nb_bases={lin.genetic_mat[-1]}")
+            lin.backward_step()
+            lin.check_fused_segments()
+            if len(lin.cur_segments) == 1:
+                print("COALESCENT at time ", lin.back_time)
+                lin.write_data(filename)
+                break
+        lin.write_data(filename)
+        #lin.write_all_distrib(filename2)
+    else:
+        print("already computed")
+
+
+pool = multiprocessing.Pool(8)
+pool.map(simulation, sweeps)
